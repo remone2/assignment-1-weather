da66096599b801225c7e8322069614d9ab03828d
remone2 <jamodinm@yahoo.com>
2021-12-12 16:44:50 -0600

    created get latitude and longtitude function

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
-
-
+function getLatAndLon() {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject("Error, cannot find your location.");
+    } else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        resolve({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+      });
+    }
+  });
+}
