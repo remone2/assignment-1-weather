a7b0c0a1c595ba4fe0749869ab0df946dcf13358
remone2 <jamodinm@yahoo.com>
2021-12-12 16:59:10 -0600

    added get api link and setup using it for later

--- src/app.js
+++ src/app.js
@@ -12,3 +12,34 @@ function getLatAndLon() {
     }
   });
 }
+
+function getWeather(source) {
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        resolve(JSON.parse(request.response));
+      } else if (request.readyState === 4) {
+        reject(`Something went wrong: ${request.status}`);
+      }
+    });
+    request.open("GET", source);
+    request.send();
+  });
+}
+
+getLatAndLon().then((data) => {
+  getWeather(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=bf09db183fdc915aa7c996d8654bf86f`
+  )
+    .then((data2) => handleWeatherData(data2))
+    .catch((err) => console.log(err));
+});
+
+getLatAndLon().then((data) => {
+  getWeather(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${data.lat}&lon=${data.lon}&appid=bf09db183fdc915aa7c996d8654bf86f`
+  )
+    .then((data2) => handleForecastData(data2))
+    .catch((err) => console.log(err));
+});
